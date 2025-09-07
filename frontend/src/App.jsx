bfcc5144103f11ae3877a0585d5cd75f9c5c9376
Developerr18 <dineshnikum1@gmail.com>
2025-09-07 11:40:15 +0530

    create Hero section UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
+import Home from "./pages/home/home";
+import Cart from "./pages/cart/cart";
+import PlaceOrder from "./pages/placeOrder/placeOrder";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
