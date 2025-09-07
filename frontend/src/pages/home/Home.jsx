52be352a73f47bb4f6a0b8bb20d615800fc2b5a7
Developerr18 <dineshnikum1@gmail.com>
2025-09-07 12:00:42 +0530

    create explore-menu UI

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import ExploreMenu from "../../components/exploreMenu/ExploreMenu";
 import Header from "../../components/header/Header";
 import "./Home.css";
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   );
 };
