5ad96ec2daace1da0d091d75520ce65b8ee43186
Developerr18 <dineshnikum1@gmail.com>
2025-09-07 16:00:57 +0530

    created footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,21 @@ import Navbar from "./components/navbar/navbar";
 import Home from "./pages/home/home";
 import Cart from "./pages/cart/cart";
 import PlaceOrder from "./pages/placeOrder/placeOrder";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
